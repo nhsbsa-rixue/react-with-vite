06a4ef901817d40747d88371c9fa1cba6559dbc1
Riwang Xue <rixue@nhsbsa.nhs.uk>
2025-01-15 12:22:50 +0000

    Vite build command

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,22 @@ const Home = () => {
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
+      
+      <section id="intro">
+        <h2>Introduction</h2>
+        <p>This is the introduction section of the Home Page.</p>
+      </section>
+      
+      <section id="products">
+        <h2>Our Products</h2>
+        <p>Here are some of our featured products.</p>
+        {/* Add product details here */}
+      </section>
+      
+      <footer>
+        <h2>Footer</h2>
+        <p>Contact us at contact@foreverflowers.com</p>
+      </footer>
     </div>
   );
 };
